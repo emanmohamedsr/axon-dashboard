c86aeb98a1735ed835eb3696b4f2fe2550a3723b
Eman Mohamed <emanmohamedsr72@gmail.com>
2026-01-24 23:41:02 +0200

    fix: build issues --correct casing for KanbanBoard import and improve hexToRgb function formatting

--- src/features/calendar/lib/colorsUtils.ts
+++ src/features/calendar/lib/colorsUtils.ts
@@ -25,16 +25,16 @@ export const colorMap: Record<string, string> = {
 const hexToRgb = (hex: string) => {
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 	const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+	hex = hex.replace(shorthandRegex, (_, r, g, b) => r + r + g + g + b + b);
 
 	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result
-		? {
+	return result ?
+			{
 				r: parseInt(result[1], 16),
 				g: parseInt(result[2], 16),
 				b: parseInt(result[3], 16),
-		  }
-		: null;
+			}
+		:	null;
 };
 
 // 3. The Main Function
--- src/features/tasks/components/index.ts
+++ src/features/tasks/components/index.ts
@@ -3,4 +3,4 @@ export { default as TaskTable } from "./table/TaskTable";
 export { default as TasksView } from "./TasksView";
 export { default as TaskDeleteAlert } from "./TaskDeleteAlert";
 export { default as TaskEditAddDialog } from "./TaskEditAddDialog";
-export { default as KanbanBoard } from "./board/KanbanBoard";
+export { default as KanbanBoard } from "./board/kanbanBoard";
